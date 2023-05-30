ffebc102f9f0d56d1af5de21d9d1e52ff77d5928
123-Shi Li <904918414@qq.com>
2023-05-30 22:37:33 +0800

    add inst for dummy

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -23,7 +23,7 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S,
+  TYPE_I, TYPE_U, TYPE_S,TYPE_J,
   TYPE_N, // none
 };
 
@@ -32,6 +32,18 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+/*#define immJ() do { *imm = SEXT(( \
+				(BITS(i, 31, 31) << 19) | \
+				BITS(i, 30, 21) | \
+				(BITS(i, 20, 20) << 10) | \
+				(BITS(i, 19, 12) << 11)  \
+				) << 1, 21); Log(ANSI_FG_CYAN "%#1lx\n" ANSI_NONE, *imm); } while(0)*/
+#define immJ() do { *imm = SEXT(( \
+(BITS(i, 31, 31) << 19) | \
+BITS(i, 30, 21) | \
+(BITS(i, 20, 20) << 10) | \
+(BITS(i, 19, 12) << 11) \
+) << 1, 21); Log(ANSI_FG_CYAN "%#lx\n" ANSI_NONE, *imm); } while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
@@ -42,6 +54,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
+	case TYPE_J:				   immJ(); break;
   }
 }
 
@@ -57,6 +70,11 @@ static int decode_exec(Decode *s) {
 }
 
   INSTPAT_START();
+
+  INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(rd) = src1 + imm);
+  INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, R(rd) = s->pc + 4; s->dnpc = (src1+imm)&~(word_t)1);
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(rd) = s->pc + 4; s->dnpc = s->pc; s->dnpc += imm);
+
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
   INSTPAT("??????? ????? ????? 011 ????? 00000 11", ld     , I, R(rd) = Mr(src1 + imm, 8));
   INSTPAT("??????? ????? ????? 011 ????? 01000 11", sd     , S, Mw(src1 + imm, 8, src2));
